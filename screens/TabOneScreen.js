2f487a8232fd087abe15dce2a6a27daf35e054ff
anas599 <anas599@yahoo.com>
2024-01-16 20:26:40 -0500

    Refactor TabOneScreen.js and rename TimePickerExample to TimePicker

--- screens/TabOneScreen.js
+++ screens/TabOneScreen.js
@@ -1,43 +1,8 @@
-// import { StyleSheet } from "react-native";
-
-// import EditScreenInfo from "../components/EditScreenInfo";
-// import { Text, View } from "../components/Themed";
-
-// export default function TabOneScreen() {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.title}>Tab One</Text>
-//       <View
-//         style={styles.separator}
-//         lightColor="#eee"
-//         darkColor="rgba(255,255,255,0.1)"
-//       />
-//       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   title: {
-//     fontSize: 20,
-//     fontWeight: "bold",
-//   },
-//   separator: {
-//     marginVertical: 30,
-//     height: 1,
-//     width: "80%",
-//   },
-// });
 import React, { useState } from "react";
 import { View, Text, Button } from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 
-const TimePickerExample = () => {
+const TimePicker = () => {
   const [startTime, setStartTime] = useState(null);
   const [endTime, setEndTime] = useState(null);
   const [hoursDifference, setHoursDifference] = useState(null);
@@ -105,4 +70,4 @@ const TimePickerExample = () => {
   );
 };
 
-export default TimePickerExample;
+export default TimePicker;
