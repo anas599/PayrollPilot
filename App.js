3c7cf9ee779d951e9815e53be36452a574e53b4d
anas599 <anas599@yahoo.com>
2024-01-21 19:58:05 -0500

    Add TimeContext to App.js

--- App.js
+++ App.js
@@ -6,19 +6,22 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { useLoadedAssets } from "./hooks/useLoadedAssets";
 import Navigation from "./navigation";
 import { useColorScheme } from "react-native";
-
+import { TimeContext } from "./context/TimeContext";
+import { useState } from "react";
 export default function App() {
   const isLoadingComplete = useLoadedAssets();
   const colorScheme = useColorScheme();
-
+  const [hoursDifference, setHoursDifference] = useState(null);
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <TimeContext.Provider value={{ hoursDifference, setHoursDifference }}>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </TimeContext.Provider>
     );
   }
 }
